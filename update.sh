6ffe251095e41ac4c29aa3f99af2be6a2187f4f1
mohramadan911 <41170602+mohramadan911@users.noreply.github.com>
2021-11-24 02:14:14 +0300

    Upload Source Code

---
+++ update.sh
@@ -0,0 +1,1 @@
+aws cloudformation update-stack --stack-name Udagram --template-body file://Our-Infra-VPC.yml  --parameters file://Our-Infra-Vpc.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-1
